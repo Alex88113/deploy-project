6af97d01dbdbfff82aacc724e31c9b589ad046c2
Alex88113 <sanya.kucheryavvkh@bk.ru>
2025-12-25 12:47:31 +0300

    Create test_corutines.py

---
+++ test_corutines.py
@@ -0,0 +1,58 @@
+import asyncio
+
+import pytest_asyncio
+import pytest
+from loguru import logger
+
+from task import (
+    processing_users_data,
+    delayed_message,
+    adding_number
+)
+
+class TestingCorutines:
+    @pytest_asyncio.fixture
+    async def test_users_data(self):
+        return {
+            'username': 'Shura'
+        }
+
+    @pytest_asyncio.fixture
+    async def test_data(self):
+        return {
+         'message': 'Привет, асинхронность!',
+         'delay': 0.1
+      }
+
+    @pytest_asyncio.fixture
+    async def test_value_numbers(self):
+        return {
+        'number1': 23.343,
+        'number2': 234
+    }
+
+    @pytest.mark.asyncio
+    async def test_processing_name(self, test_users_data):
+        result = await processing_users_data(test_users_data['username'])
+        assert result == {
+        'username': 'Shura'
+    }
+        logger.info('Первый тест прошел успешно!')
+
+    @pytest.mark.asyncio
+    async def test_adding_numbers(self, test_value_numbers):
+        result = await adding_number(
+        test_value_numbers['number1'],
+        test_value_numbers['number2']
+        )
+        assert result == 257.343
+        logger.info('Второй тест прошел успешно!')
+
+    @pytest.mark.asyncio
+    async def test_delayed_message(self, test_data):
+        result = await delayed_message(
+            test_data['message'],
+            test_data['delay']
+        )
+        assert result == test_data['message']
+        logger.info('Третий тест прошел успешно!')
