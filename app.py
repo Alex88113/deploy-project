c633cda1e45064ef5ca6bda82815ea4d6066255d
Alex88113 <sanya.kucheryavvkh@bk.ru>
2025-12-25 12:33:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 def sum(number1: int | float, number2: int | float) -> int | float:
     return number1 + number2
 
-def multy(number1: int | float, number2: int | float) -> int | float:
+def multy(number1: int, number2: int) -> int:
     return number1 * number2
