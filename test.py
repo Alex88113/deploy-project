1388785ff9d64f7d542afa4209f5346698d94a15
Alex88113 <sanya.kucheryavvkh@bk.ru>
2025-12-25 12:30:24 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
-from app import sum
+from app import sum, multy
 
 def test_sum():
     assert sum(2, 3) == 5
     assert sum(-1, 1) == 0
     print("All tests passed!")
 
+def test_multy():
+    assert multy(2, 4) == 10
 if __name__ == "__main__":
     test_sum()
